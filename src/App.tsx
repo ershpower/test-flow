fd4a92055800637487183a540e05251d01fef180
vershov <vershov@clink.tech>
2024-07-26 14:23:37 +0400

    add man

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { ChangeEvent, useEffect, useState } from 'react';
 import './App.css';
 import Bezie from './components/Bezie/Bezie';
 import { generateRandomNumberInRange } from './utils';
+import Man from './assets/man.webp';
 
 const DELAY = 200;
 const diffX = 40;
@@ -63,6 +64,16 @@ function App() {
             <div className="container">
                 <div className="y"></div>
                 <div className="x"></div>
+                <div className="man">
+                    <img
+                        src={Man}
+                        alt="man"
+                        className="man"
+                        style={{
+                            transform: `translate(${point2.x - 20}px, -${point2.y}px)`,
+                        }}
+                    />
+                </div>
                 <div className="graphic">
                     <Bezie point1={point1} point2={point2} />
                 </div>
---
+++ src/declaration.d.ts
@@ -0,0 +1,5 @@
+// declaration.d.ts
+declare module '*.webp' {
+    const value: string;
+    export default value;
+}
